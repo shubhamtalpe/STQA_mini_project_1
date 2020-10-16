eb170ec2c9279218e403fafdea65cc01eb640787
shubhamtalpe <shubhamtalpeofficial@gmail.com>
2020-10-16 11:37:48 +0530

    Run on console using database= standaloneApp username= root password= root@123

--- src/main/java/standaloneApp/Main.java
+++ src/main/java/standaloneApp/Main.java
@@ -1,9 +1,26 @@
 package standaloneApp;
 
-public class Main {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    @Autowired
+    private standaloneApp.service.StudentService studentService;
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        System.out.println(studentService.getAllowedNumberOfBooks());
+        System.exit(0);
     }
 }
