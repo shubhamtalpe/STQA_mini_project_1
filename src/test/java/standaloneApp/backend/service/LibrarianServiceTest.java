b621db67ae913982a786bbdd0544208ee5e72664
shubhamtalpe <shubhamtalpeofficial@gmail.com>
2020-11-24 19:02:24 +0530

    issue and return book test done

--- src/test/java/standaloneApp/backend/service/LibrarianServiceTest.java
+++ src/test/java/standaloneApp/backend/service/LibrarianServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,13 +17,48 @@ class LibrarianServiceTest {
     LibrarianService librarianService;
 
     @Test
-    void issueBook() {
-
+    void issueMoreBooksThanAllowed() {
+        assertEquals(librarianService.issueBook("0100", "00004", new java.util.Date().toString()), "Can not issue more books");
     }
+
+    @Test
+    void issueInvalidBook() {
+        assertEquals(librarianService.issueBook("0100", "00005", new java.util.Date().toString()), "Invalid Book ID");
+    }
+
+    @Test
+    void issueBookAlreadyIssued() {
+        assertEquals(librarianService.issueBook("0010", "00001", new java.util.Date().toString()), "Book already issued");
+    }
+
+    @Test
+    void issueBook() {
+        assertEquals(librarianService.issueBook("0010", "00002", new java.util.Date().toString()), "Book Issued");
+    }
+
+    @Test
+    void issueBookWhichIsNotAvailable() {
+        assertEquals(librarianService.issueBook("0010", "00003", new java.util.Date().toString()), "Book Not Available");
+    }
+
+    @Test
+    void returnBookWhichIsNotIssued() {
+        assertEquals(librarianService.returnBook("0100", "00004", new java.util.Date().toString()), "Invalid Book ID");
+    }
+
     @Test
     void returnBook() {
-
+        assertEquals(librarianService.returnBook("0010", "00002", new java.util.Date().toString()), "Book Returned");
     }
+
+    @Test
+    void returnBookWhichIsAlreadyReturned() {
+        assertEquals(librarianService.returnBook("0010", "00001", new java.util.Date().toString()), "Book Already returned");
+    }
+
+
+    //Done till here
+
 
     @Test
     void addBook() {
