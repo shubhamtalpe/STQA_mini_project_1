2849347441a193f4ded283ca2d862dd364e3d480
soham <soham1999@gmail.com>
2020-11-24 09:22:22 +0530

    showUserDetails GUI completed.

---
+++ src/test/java/standaloneApp/frontend/GUITest.java
@@ -0,0 +1,43 @@
+package standaloneApp.frontend;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GUITest {
+
+    @org.junit.jupiter.api.Test
+    void loginPage() {
+        GUI gui = new GUI();
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void issueBookPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnBookPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addBookPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMultipleBookPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addUserPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMultipleUserPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showUserDetailsPage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showSearchResults() {
+    }
+}
